1b2c5a944ec48a2e140f26a1a6ed6d039f103961
Amaria <61030238@kmitl.ac.th>
2019-09-01 19:24:18 +0700

    2nd  year semester 1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Year 255x semster 1, I am an Eng freshman");
             Console.WriteLine("Year 255x semster 1, I learn to program in c sharp");
+            Console.WriteLine("Year 2560 semster 1, I start my programmer life");
         }
     }
 }
