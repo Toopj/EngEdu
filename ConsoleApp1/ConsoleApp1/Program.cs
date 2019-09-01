cf6861e14fb44ca49e42c24d8b886f065afcb308
Amaria <61030238@kmitl.ac.th>
2019-09-01 19:08:54 +0700

    first year semester 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 255x semster 1, I am an Eng freshman");
+        }
+    }
+}
